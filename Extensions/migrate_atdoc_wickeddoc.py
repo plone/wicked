7bb6e2883485bd42f01474254c254ad7906542b1
rob <rob@localhost>
2005-09-24 03:26:45 +0000

    * we're only doing one migration, no need for a loop
    * check that the types haven't already been switched before trying
      to switch them
    
    
    svn path=/wicked/trunk/; revision=28423

--- Extensions/migrate_atdoc_wickeddoc.py
+++ Extensions/migrate_atdoc_wickeddoc.py
@@ -5,29 +5,31 @@ from Products.CMFCore.utils import getToolByName
 def migrate_atdoc_wickeddoc(self):
     catalog = getToolByName(self, 'portal_catalog')
     portal = getToolByName(self, 'portal_url').getPortalObject()
-    migrators = (WickedDocMigrator,)
     out = []
 
-    for migrator in migrators:
-        out.append('*** Migrating %s to %s ***\n' % (migrator.src_portal_type,
-                                                     migrator.dst_portal_type))
-        try:
-            w = CatalogWalker(migrator, portal) # ATCT-0.2
-        except AttributeError:
-            w = CatalogWalker(portal, migrator) # ATCT-1.0
-        w_result = w.go()
-        if type(w_result) == type(''):
-            out.append(w_result) # ATCT-0.2
-        else:
-            out.append('%s Migrated\n' % migrator.src_portal_type)
+    migrator = WickedDocMigrator
+    out.append('*** Migrating %s to %s ***\n' % (migrator.src_portal_type,
+                                                 migrator.dst_portal_type))
+    try:
+        w = CatalogWalker(migrator, portal) # ATCT-0.2
+    except AttributeError:
+        w = CatalogWalker(portal, migrator) # ATCT-1.0
+    w_result = w.go()
+    if type(w_result) == type(''):
+        out.append(w_result) # ATCT-0.2
+    else:
+        out.append('%s Migrated\n' % migrator.src_portal_type)
 
-        wf = getToolByName(self, 'portal_workflow')
-        count = wf.updateRoleMappings()
-        out.append('Workflow: %d object(s) updated.' % count)
-    
-        catalog.refreshCatalog(clear=1)
-        out.append('Portal catalog updated.')
+    wf = getToolByName(self, 'portal_workflow')
+    count = wf.updateRoleMappings()
+    out.append('Workflow: %d object(s) updated.' % count)
 
+    catalog.refreshCatalog(clear=1)
+    out.append('Portal catalog updated.')
+
+    ttool = getToolByName(self, 'portal_types')
+    doc_fti = ttool.getTypeInfo('Document')
+    if doc_fti.Metatype() != 'WickedDoc':
         atct_tool = getToolByName(self, 'portal_atct')
         get_transaction().commit(1)
         atct_tool._changePortalTypeName('Document', 'ATDocument',
@@ -39,5 +41,4 @@ def migrate_atdoc_wickeddoc(self):
                                         title='Page')
         out.append('Document types switched')
 
-        return '\n'.join(out)
-
+    return '\n'.join(out)
