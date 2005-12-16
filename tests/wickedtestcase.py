a8042c649568198f6039481e2a90a119ac4b1f78
whit <whit@localhost>
2005-12-16 20:19:21 +0000

    added more verbose methods to wickedtestcase that display the wicked link upon failure.
    also added a soft dependency on lxml for cleaning up wicked link output
    
    
    svn path=/wicked/trunk/; revision=28534

--- tests/wickedtestcase.py
+++ tests/wickedtestcase.py
@@ -1,15 +1,22 @@
-from Testing                 import ZopeTestCase
+import os
+from cStringIO import StringIO
+from Testing import ZopeTestCase
 from Products.CMFCore.utils  import getToolByName
 from Products.PloneTestCase import ptc 
 #import cmftc
 #from Products.Archetypes.tests.ArchetypesTestCase import ArcheSiteTestCase
 from Products.wicked.Extensions.Install import install as installWicked
 
+# monkey patch config into ironicwiki schema
+from Products.wicked.example.ironicwiki import schema
+schema['body'].wicked_macro='wicked_test'
+
 import Products.wicked.config as config
 from Products.wicked.utils import parseDepends, doc_file
 from Products.wicked.api import titleToNormalizedId
 from Products.filter.tests.filtertestcase import fivezcml, filterzcml, setUp, tearDown, zcml, FilterTestCase
 import Products.wicked
+
 
 def setupCA():
     tearDown()
@@ -50,10 +57,11 @@ def makeContent(container, id, portal_type, **kw):
 TITLE1 = "Cop Shop"
 TITLE2 = 'DMV Computer has died'
 
+USELXML = False
+
 # This is the test case. You will have to add test_<methods> to your
 # class in order to assert things about your Product.
 class WickedTestCase(FilterTestCase):
-#class WickedTestCase(cmftc.SiteTestCase):
     
     def afterSetUp(self):
         super(WickedTestCase, self).afterSetUp()
@@ -66,10 +74,62 @@ class WickedTestCase(FilterTestCase):
                                  titleToNormalizedId(TITLE2),
                                  self.wicked_type, title=TITLE2)
 
+    def strip(self, text):
+        """
+        if lxml is available, use it to strip out whitespace
+        """
+        text = text.strip()
+        if not text:
+            return text
+        try:
+            from lxml import etree
+        except ImportError:
+            return text
+
+        # ganked from zopt and sfive
+
+        xsltfile = os.path.join(os.path.dirname(__file__), 'strip.xsl')
+        xslt = file(xsltfile)
+        xslt_doc = etree.parse(xslt)
+        style = etree.XSLT(xslt_doc)
+
+        textfile = StringIO(text)
+        doc = etree.parse(textfile)
+        result = style.apply(doc)
+        return style.tostring(result)
 
     def getRenderedWickedField(self, doc):
         fieldname = self.wicked_field
-        return doc.getField(fieldname).getAccessor(doc)()
+        text = doc.getField(fieldname).getAccessor(doc)()
+        return self.strip(text)
+
+    def failIfAddLink(self, doc):
+        """ does wicked field text contain a wicked-generated add link? """
+        # XXX make test stronger, support looking for specific links
+        home_url= doc.absolute_url()
+        text = self.getRenderedWickedField(doc)
+        if home_url in text:
+            self.fail("%s FOUND:\n\n %s" %(home_url, text))
+
+    def failUnlessAddLink(self, doc):
+        """ does wicked field text contain a wicked-generated add link? """
+        # XXX make test stronger, support looking for specific links
+        home_url= doc.absolute_url()
+        text = self.getRenderedWickedField(doc)
+        if not home_url in text:
+            self.fail("%s NOT FOUND:\n\n %s" %(home_url, text))
+
+    def failIfWickedLink(self, doc, dest):
+        dest = dest.absolute_url()
+        text = self.getRenderedWickedField(doc)
+        if dest in text:
+            self.fail("%s FOUND:\n\n %s" %(dest, text))
+
+    def failUnlessWickedLink(self, doc, dest):
+        dest = dest.absolute_url()
+        text = self.getRenderedWickedField(doc)
+        if not dest in text:
+            self.fail("%s NOT FOUND:\n\n %s" %(dest, text))
 
     def hasAddLink(self, doc):
         """ does wicked field text contain a wicked-generated add link? """
