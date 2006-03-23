b183e2a48c82166f25a23816c83ac99c5e7421da
whit <whit@localhost>
2006-03-23 00:07:29 +0000

    initial commit
    svn path=/wicked/branches/componentization/; revision=28705

--- browser/link.py
+++ browser/link.py
@@ -1,9 +1,14 @@
 from Products.Five import BrowserView
+from Products.wicked.lib.utils import memoizedproperty
+from interfaces import WickedLink
+
+_marker=object()
 
 class WickedLink(BrowserView):
     """
-    
+    renderer for wicked links
     """
+    
     def __init__(self, context, request):
         self._context = (context,)
         self.request = request
@@ -12,4 +17,27 @@ class WickedLink(BrowserView):
     def context(self):
         return self._context[0]
 
+    @memoizedproperty
+    def howmany(self):
+        return len(self.links)
 
+    @property
+    def multiple(self):
+        return self.howmany > 1
+
+    @memoizedproperty
+    def links(self):
+        return self._links
+
+    @property
+    def singlelink(self):
+        return self.links[0]
+
+    @clearmemo
+    def load(self, links, chunk, section=_marker):
+        self._links = links
+        self.chunk = chunk
+        if section is _marker:
+            assert self.section
+            return
+        self.section = section
