2858fb56148f8c5e2579c0ba6597d8108c3e327f
whit <whit@localhost>
2006-03-29 16:11:11 +0000

    abstracting out Five specific mixin
    svn path=/wicked/branches/componentization/; revision=28717

--- browser/link.py
+++ browser/link.py
@@ -1,13 +1,14 @@
 from Products.Five import BrowserView
-from Products.wicked.lib.utils import memoizedproperty
+from Products.wicked.lib.utils import memoize, memoizedproperty, clearbefore, counter
+
 from interfaces import WickedLink
 
 _marker=object()
 
-class WickedLink(BrowserView):
-    """
-    renderer for wicked links
-    """
+class BasicLink(object):
+    """renderer for wicked links"""
+    section = _marker
+    counter=counter()
     
     def __init__(self, context, request):
         self._context = (context,)
@@ -21,23 +22,47 @@ class WickedLink(BrowserView):
     def howmany(self):
         return len(self.links)
 
-    @property
+    @memoizedproperty
     def multiple(self):
         return self.howmany > 1
 
     @memoizedproperty
     def links(self):
+        """
+        calculates urls
+        """
+        for link in self._links:
+            link['url'] = self.request.physicalPathToURL(link['path'], 0)
         return self._links
 
     @property
     def singlelink(self):
         return self.links[0]
 
-    @clearmemo
+    @property
+    def count(self):
+        return self.counter.next()
+
+    @clearbefore
     def load(self, links, chunk, section=_marker):
         self._links = links
         self.chunk = chunk
-        if section is _marker:
-            assert self.section
-            return
+        assert section is not _marker, 'Must supply a section name'
         self.section = section
+
+
+class BasicFiveLink(BrowserView, BasicLink):
+    """
+    Five prepared link implementation
+    """
+
+import unittest
+from zope.testing import doctest
+optionflags = doctest.REPORT_ONLY_FIRST_FAILURE | doctest.ELLIPSIS
+def test_suite():
+    return unittest.TestSuite((
+        doctest.DocFileSuite('link.txt', optionflags=optionflags),
+        ))
+
+if __name__=="__main__":
+    unittest.TextTestRunner().run(test_suite())
