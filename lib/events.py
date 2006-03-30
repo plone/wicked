b882195afeb7ae34efba791c7eb5f645914b1930
whit <whit@localhost>
2006-03-30 20:41:43 +0000

    remove some cruft
    svn path=/wicked/branches/componentization/; revision=28740

--- lib/events.py
+++ lib/events.py
@@ -1,20 +1,5 @@
 from zope.app.event.interfaces import IObjectEventw
 from zope.interface import implements, Interface
-
-class IWickedAddEvent(IObjectEvent):
-    """
-    
-    """
-
-
-
-class WickedAddEvent(object):
-    """ An object has been added via wicked """
-    implements(IWickedAddEvent)
-    
-    def __init__(self, obj):
-        self.object = obj
-        
     
 def handleTargetDeletion(obj, event):
     """
