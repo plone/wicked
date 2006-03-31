10f3521aefb491e677fac5049b532dbfa2e5abfb
whit <whit@localhost>
2006-03-31 06:18:37 +0000

    see last commit
    svn path=/wicked/branches/componentization/; revision=28743

--- lib/events.py
+++ lib/events.py
@@ -1,13 +1,28 @@
-from zope.app.event.interfaces import IObjectEventw
-from zope.interface import implements, Interface
-    
-def handleTargetDeletion(obj, event):
+from Products.wicked import config, utils
+
+def handleTargetDeletion(ref, event):
     """
+    Invalidate any pointer before object deletion
     """
-    import pdb; pdb.set_trace()
+    target = ref.getTargetObject()
+    wicked = utils.getFilter(target)
+    wicked.unlink(ref.sourceUID)
 
 
 def handleTargetMove(obj, event):
     """
+    when a target of a link is moved, or renamed we need to notify any
+    objects that may be caching pointers
     """
-    import pdb; pdb.set_trace()
+    #import pdb; pdb.set_trace()
+    refs=obj.getRefs(relationship=config.BACKLINK_RELATIONSHIP)
+    for ref in refs:
+        wicked = utils.getFilter(obj)
+        uid = obj.UID()
+        path = '/'.join(obj.getPhysicalPath())
+        data = dict(path=path,
+                    icon=obj.getIcon(),
+                    uid=uid)
+        wicked.cache.uidset(uid, data)
+
+    
