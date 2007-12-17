308fba145b2eb6cac933175d6cc207dc42c7840a
Whit Morriss <whit@openplans.org>
2007-12-17 17:20:26 +0000

    comment out now unused normalizeISO stuff
    svn path=/wicked/branches/1.0-unicode-fix/; revision=55729

--- lib/normalize.py
+++ lib/normalize.py
@@ -12,46 +12,44 @@
 __authors__ = 'Anders Pearson <anders@columbia.edu>'
 __docformat__ = 'restructuredtext'
 
-import re
-import unittest
 from unaccent import unaccented_map
+import re
+import types
+import unittest
 
-mapping = {138: 's', 140: 'OE', 142: 'z', 154: 's', 156: 'oe', 158: 'z', 159: 'Y', 
-192: 'A', 193: 'A', 194: 'A', 195: 'A', 196: 'A', 197: 'a', 198: 'E', 199: 'C', 
-200: 'E', 201: 'E', 202: 'E', 203: 'E', 204: 'I', 205: 'I', 206: 'I', 207: 'I', 
-208: 'D', 209: 'n', 211: 'O', 212: 'O', 214: 'O', 216: 'O', 217: 'U', 218: 'U', 
-219: 'U', 220: 'U', 221: 'y', 223: 'ss', 224: 'a', 225: 'a', 226: 'a', 227: 'a', 
-228: 'a', 229: 'a', 230: 'e', 231: 'c', 232: 'e', 233: 'e', 234: 'e', 235: 'e', 
-236: 'i', 237: 'i', 238: 'i', 239: 'i', 240: 'd', 241: 'n', 243: 'o', 244: 'o', 
-246: 'o', 248: 'o', 249: 'u', 250: 'u', 251: 'u', 252: 'u', 253: 'y', 255: 'y'}
+## mapping = {138: 's', 140: 'OE', 142: 'z', 154: 's', 156: 'oe', 158: 'z', 159: 'Y', 
+## 192: 'A', 193: 'A', 194: 'A', 195: 'A', 196: 'A', 197: 'a', 198: 'E', 199: 'C', 
+## 200: 'E', 201: 'E', 202: 'E', 203: 'E', 204: 'I', 205: 'I', 206: 'I', 207: 'I', 
+## 208: 'D', 209: 'n', 211: 'O', 212: 'O', 214: 'O', 216: 'O', 217: 'U', 218: 'U', 
+## 219: 'U', 220: 'U', 221: 'y', 223: 'ss', 224: 'a', 225: 'a', 226: 'a', 227: 'a', 
+## 228: 'a', 229: 'a', 230: 'e', 231: 'c', 232: 'e', 233: 'e', 234: 'e', 235: 'e', 
+## 236: 'i', 237: 'i', 238: 'i', 239: 'i', 240: 'd', 241: 'n', 243: 'o', 244: 'o', 
+## 246: 'o', 248: 'o', 249: 'u', 250: 'u', 251: 'u', 252: 'u', 253: 'y', 255: 'y'}
 
 
-def normalizeISO(text=""):
-    fixed = []
-    for c in list(text):
-        if ord(c) < 256:
-            c = mapping.get(ord(c),c)
-        else:
-            c = "%x" % ord(c)
-        fixed.append(c)
-    return "".join(fixed)
+## def normalizeISO(text=""):
+##     fixed = []
+##     for c in list(text):
+##         if ord(c) < 256:
+##             c = mapping.get(ord(c),c)
+##         else:
+##             c = "%x" % ord(c)
+##         fixed.append(c)
+##     return "".join(fixed)
 
 
 pattern1 = re.compile(r"^([^\.]+)\.(\w{,4})$")
 #pattern2 = re.compile(r'r"([\W\-]+)"')
 pattern2 = re.compile(r'([\W\-]+)')
-#non_alpha = re.compile(r'[\W\-]+')
 non_alpha = re.compile(r'[^a-zA-Z0-9~]+')
 u_esc = re.compile(r'\\u')
 
-map = unaccented_map()
-import types
-def titleToNormalizedId(title=""):
+def titleToNormalizedId(title="", umap=unaccented_map()):
     title = title.lower()
     title = title.strip()
     #title = normalizeISO(title)
     if title and isinstance(title, types.UnicodeType):
-        title = title.translate(map).encode("ascii", 'backslashreplace')
+        title = title.translate(umap).encode("ascii", 'backslashreplace')
     base = title
     ext = ""
     m = pattern1.match(title)
