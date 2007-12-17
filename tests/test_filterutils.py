3e9a32e6eb8d0947c763e6b549bfa1a7df42f61a
Whit Morriss <whit@openplans.org>
2007-12-17 16:29:12 +0000

    update test runner to include unicode tests
    svn path=/wicked/branches/1.0-unicode-fix/; revision=55724

--- tests/test_filterutils.py
+++ tests/test_filterutils.py
@@ -1,4 +1,6 @@
-import os, sys, unittest
+import os
+import sys
+import unittest
 
 base = "../lib"
 try: base = os.path.abspath(os.path.join(os.path.dirname(__file__), base))
@@ -6,7 +8,12 @@ except: pass
 
 sys.path.insert(0, base)
 
-from utils import test_suite
+from utils import test_suite as util_suite
+from normalize import NormTestCase
+
+def test_suite():
+    return unittest.TestSuite((util_suite(), unittest.makeSuite(NormTestCase)))
+
 if __name__ == '__main__':
-    unittest.TextTestRunner().run(test_suite())
+    unittest.TextTestRunner().run(test_suite)
 
