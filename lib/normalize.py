2ea6ded23bb982ef6dbde7067ebd1061c110bfda
Whit Morriss <whit@openplans.org>
2007-12-27 17:59:38 +0000

    unicode sanity cleanup
    - compile regex
    - use $ as \x escape
    - transform intention ~ 
    
    svn path=/wicked/branches/1.0-unicode-fix/; revision=56177

--- lib/normalize.py
+++ lib/normalize.py
@@ -9,45 +9,26 @@
 #
 ##########################################################
 
-__authors__ = 'Anders Pearson <anders@columbia.edu>'
-__docformat__ = 'restructuredtext'
+#__authors__ = 'Anders Pearson <anders@columbia.edu>'
+#__docformat__ = 'restructuredtext'
 
 from unaccent import unaccented_map
 import re
 import types
 import unittest
 
-## mapping = {138: 's', 140: 'OE', 142: 'z', 154: 's', 156: 'oe', 158: 'z', 159: 'Y', 
-## 192: 'A', 193: 'A', 194: 'A', 195: 'A', 196: 'A', 197: 'a', 198: 'E', 199: 'C', 
-## 200: 'E', 201: 'E', 202: 'E', 203: 'E', 204: 'I', 205: 'I', 206: 'I', 207: 'I', 
-## 208: 'D', 209: 'n', 211: 'O', 212: 'O', 214: 'O', 216: 'O', 217: 'U', 218: 'U', 
-## 219: 'U', 220: 'U', 221: 'y', 223: 'ss', 224: 'a', 225: 'a', 226: 'a', 227: 'a', 
-## 228: 'a', 229: 'a', 230: 'e', 231: 'c', 232: 'e', 233: 'e', 234: 'e', 235: 'e', 
-## 236: 'i', 237: 'i', 238: 'i', 239: 'i', 240: 'd', 241: 'n', 243: 'o', 244: 'o', 
-## 246: 'o', 248: 'o', 249: 'u', 250: 'u', 251: 'u', 252: 'u', 253: 'y', 255: 'y'}
-
-
-## def normalizeISO(text=""):
-##     fixed = []
-##     for c in list(text):
-##         if ord(c) < 256:
-##             c = mapping.get(ord(c),c)
-##         else:
-##             c = "%x" % ord(c)
-##         fixed.append(c)
-##     return "".join(fixed)
-
-
 pattern1 = re.compile(r"^([^\.]+)\.(\w{,4})$")
-#pattern2 = re.compile(r'r"([\W\-]+)"')
 pattern2 = re.compile(r'([\W\-]+)')
-non_alpha = re.compile(r'[^a-zA-Z0-9~]+')
+esc_char = re.compile(r'[~$]')
+non_alpha = re.compile(r'[^a-zA-Z0-9~$]+')
 u_esc = re.compile(r'\\u')
+trailing_slash = re.compile(r"-+$")
+leading_slash = re.compile(r"^-+")
 
 def titleToNormalizedId(title="", umap=unaccented_map()):
     title = title.lower()
     title = title.strip()
-    #title = normalizeISO(title)
+
     if title and isinstance(title, types.UnicodeType):
         title = title.translate(umap).encode("ascii", 'backslashreplace')
     base = title
@@ -58,12 +39,17 @@ def titleToNormalizedId(title="", umap=unaccented_map()):
         ext = m.groups()[1]
     parts = pattern2.split(base)
 
-    # speed these up by precompiling
     slug = base
+    slug = slug.replace('~', '-')
     slug = slug.replace('\u', '~')
-    slug = non_alpha.sub("-", slug) # replace non-alphanumeric characters with dashes
-    slug = re.sub(r"^-+","", slug)     # trim leading dashes
-    slug = re.sub(r"-+$","", slug)     # trim trailing dashes
+    slug = slug.replace('\\x', '$')
+
+    # replace non-alphanumeric characters with dashes
+    slug = non_alpha.sub("-", slug)
+
+    # trim slashes
+    slug = leading_slash.sub("", slug)
+    slug = trailing_slash.sub("", slug)
     if ext != "":
         slug = slug + "." + ext
     return slug
@@ -77,11 +63,13 @@ class NormTestCase(unittest.TestCase):
     (u'About folder.gif', u'about-folder.gif'),
     (u"laboratoire de g\xe9omatique", "laboratoire-de-geomatique"),
     (u'Eksempel \xe6\xf8\xe5 norsk \xc6\xd8\xc5', u'eksempel-aeoea-norsk-aeoea'), 
-    (u'\u9ad8\u8054\u5408 Chinese', u'2837821-chinese'), 
-    (u'\u30a2\u30ec\u30af\u30b5\u30f3\u30c0\u30fc\u3000\u30ea\u30df Japanese', u'23987643-japanese'), 
-    (u'\uc774\ubbf8\uc9f1 Korean', u'987342-korean'), 
+    (u'\u9ad8\u8054\u5408 Chinese', u'~9ad8~8054~5408-chinese'), 
+    (u'\u30a2\u30ec\u30af\u30b5\u30f3\u30c0\u30fc\u3000\u30ea\u30df Japanese', u'~30a2~30ec~30af~30b5~30f3~30bf~30fc~3000~30ea~30df-japanese'), 
+    (u'\uc774\ubbf8\uc9f1 Korean', u'~c774~bbf8~c9f1-korean'), 
     (u'\u0e2d\u0e40\u0e25\u0e47\u0e01\u0e0b\u0e32\u0e19\u0e40\u0e14\u0e2d\u0e23\u0e4c \u0e25\u0e35\u0e21 Thai',
-     u'7265837-thai'), 
+     u'~0e2d~0e40~0e25~0e47~0e01~0e0b~0e32~0e19~0e40~0e14~0e2d~0e23~0e4c-~0e25~0e35~0e21-thai'),
+    (u'fish & chips', u'fish-and-chips'),
+    (u"~jim@work ", u"jimatwork")
     ]
 
     @classmethod
